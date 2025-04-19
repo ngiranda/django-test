a2c609bb9f73ab92d7d609d8e63fce05844307dc
Nick Giranda <ngiranda@gmail.com>
2025-04-19 15:47:40 -0400

    Registering the router in urls.py.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from todo import views
+from rest_framework import routers
+
+# create a router object
+router = routers.DefaultRouter()
+
+# register the router
+router.register(r'tasks',views.TodoView, 'task')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Add another path to the url patterns
+    # when you visit the localhost:8000/api
+    # you should be routed to the django Rest framework
+    path('api/', include(router.urls))
 ]
