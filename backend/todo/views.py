a8cf0fdf89c813a66125e2d6dd32e7a36fbcc32b
Nick Giranda <ngiranda@gmail.com>
2025-04-19 15:28:05 -0400

    Creating a view using the django rest framework and our serializer.

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from.serializers import TodoSerializer
+
+from .models import Todo
+
+class TodoView(viewsets.ModelViewSet):
+
+    # Create a serializer class and
+    # assign it to the TodoSerializer class
+    serializer_class = TodoSerializer
+
+    # Define a variable and populate it
+    # with the TodoObject list objects
+    queryset = Todo.objects.all()
