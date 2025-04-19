de0a04a4b768c9becb8315c80e0acc0fda0771fe
Nick Giranda <ngiranda@gmail.com>
2025-04-19 15:09:51 -0400

    Registering the model here.
    Note, I also used manage.py to create a super user, again following https://www.geeksforgeeks.org/integrating-django-with-reactjs-using-django-rest-framework/

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+
+    list_display = ("title", "description", "completed")
+
+# We will need to register the
+# model class and the Admin model class
+# using the register() method
+# of admin.site class
+admin.site.register(Todo, TodoAdmin)
